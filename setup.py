eccecd86d6887d6ce787987fb6a7cd926c06bffb
fyfhaolihai666 <fu194@purdue.edu>
2019-04-09 01:49:23 -0400

    zzz

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+
+setup(
+    name="commentparser",
+    version="1.0",
+    author="Yuefan Fu",
+    author_email="fuyuefan001@gmail.com",
+    url='https://github.com/fuyuefan001/htmlparser',
+    python_requires='>=3.6',
+    install_requires=[
+        'requests>=2.21.0',
+        'setuptools>=16.0',
+        'bs4>=0.0.1'
+    ],
+    zip_safe=False
+)
