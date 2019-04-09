958c7742d342969755ad9d0369fe3015f473e853
fyf <fu194@purdue.edu>
2019-04-08 21:17:50 -0400

    all

--- htmlparser.py
+++ htmlparser.py
@@ -1,24 +1,64 @@
-import requests
+# import requests
 import urllib.request
-
-
-def parse(url):
-    res = requests.get(url)
-    res.encoding = 'utf-8'
-    with open('a.txt','w') as fp:
-        fp.write(res.text)
-    print(res.text)
-
-def parse2(url):
+import html.parser
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+comment=[]
+dataslice=[]
+def url2html(url):
     file = urllib.request.urlopen(url)
     data = file.read()
     data=str(data, encoding = "utf-8")
     return (data)
-    # with open('a.txt','w',encoding="utf-8") as fp:
-    #     fp.write(data)
+
+def crawl(string):
+    soup1 = BeautifulSoup(string,'html')
+    result1 = soup1.find_all(attrs={"class":"a-expander-content reviewText review-text-content a-expander-partial-collapse-content"})
+    hp_ins=hp()
+    # hp_ins.feed(data=html)
+    # hp_ins.dataslice=[]
+    for res in result1:
+        # print(res)
+        hp_ins.dataslice = []
+        hp_ins.feed(data=str(res))
+        comment.append(hp_ins.dataslice)
+class hp(HTMLParser):
+    flg=0
+    dataslice=[]
+    def handle_starttag(self, tag, attr):
+        if tag=='span':#目标标签
+            for a in attr:
+                # print(a)
+                if a[0]=='class' and a[1]=='':
+                    self.flg=1
+                    break
+        else:
+            pass
+
+    def handle_endtag(self,tag):
+        if tag == 'span':
+            self.flg=0
+
+    def handle_data(self, data):
+        if self.flg==1:
+            # print(data)
+            self.dataslice.append(data)
+
 
 
 if __name__ == "__main__":
-    url='https://www.amazon.com/Dell-Touchscreen-Quad-Core-MaxxAudio-Thunderbolt/dp/B07MDSLT8Z/ref=sr_1_2_sspa?crid=X9Y1D4UVT9H5&keywords=dell+xps+13+9370&qid=1554756304&s=gateway&sprefix=dell+xps+13%2Caps%2C162&sr=8-2-spons&psc=1'
+    url='https://www.amazon.com/Intel-i7-8700-Desktop-Processor-Cores/product-reviews/B07598HLB4/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews'
+    # html=url2html(url)
 
-    data=parse2(url)
+    fp= open('html.txt','r')
+    html=fp.read()
+    # print(html)
+    # with open('html.txt','w',encoding="utf-8") as fp:
+    #     fp.write(html)
+
+    crawl(str(html))
+    with open('comments.txt','w') as fp:
+
+        for c in comment:
+            fp.write(str(c))
+            fp.write('\n')
